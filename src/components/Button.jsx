cc43c282976aa7a33dd3417f54f079f06f6f08d8
ApoorvaRaj123 <99551949+ApoorvaRaj123@users.noreply.github.com>
2024-03-22 21:57:57 +0530

    Add button component in components folder

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import { React } from "react";
+
+function Button() {
+  
+
+  return (
+    <div className="min-w-40 px-3 py-2 bg-zinc-100 text-black rounded-full">
+        <span className="text-sm font-semibold">Get Started</span>
+    </div>
+  )
+}
+
+export default Button;
